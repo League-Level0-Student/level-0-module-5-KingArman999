8b193097f41172540eb93b37850000043b4d869c
Computer <email@email.com>
2019-04-20 11:27:16 -0700

    ForloopGauntlet

--- src/FORLOOPGAUNLET.java
+++ src/FORLOOPGAUNLET.java
@@ -9,32 +9,43 @@ public static void main(String[] args) {
 //for (int i = 100; i > -1; i--) {
 //	System.out.println(i);
 //}
-for (int i = 2; i < 101 ; i+=2) {
-	System.out.println(i);
+//for (int i = 2; i < 101 ; i+=2) {
+//	System.out.println(i);
+//}
+//for (int i = 1; i < 100; i+=2) {
+//	System.out.println(i);
+//}
+//for (int i = 0; i < 501; i++) {
+//	if (i % 2 == 0) {
+//		System.out.println(i + " is even");
+//	}
+//		else 
+//		System.out.println(i + " is odd");
+//for (int i = 7; i < 777; i++) {
+//	if (i % 7==0) {
+//		System.out.println(i);
+//	}
+//}		
+//for (int i = 2005; i < 2020; i++) {
+//	System.out.println("In " + i + ", I was " + (i - 2005) + " year olds!" );
+//} 	
+//	for (int i = 0; i < 3; i++) {
+//		for (int j = 0; j < 3; j++) {
+//			System.out.println(i + " " + j);
+//		}
+//	}	
+//for (int i = 1; i < 10; i+=3) {
+//	for (int j = 0; j < 1; j++) {
+//		System.out.println(i + " "+ (i+1) + " " + (i+2));
+//	}
+//}
+for (int i = 1; i < 100; i+=10) {
+	for (int j = 0; j < 1; j++) {
+		System.out.println(i + " " + (i+1)+" "+(i+2)+" "+(i+3)+" "+(i+4)+" "+(i+5)+" "+(i+6)+" "+(i+7)+" "+(i+8)+" "+(i+9));
+	}
 }
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}		
+
+
 }
+}	
